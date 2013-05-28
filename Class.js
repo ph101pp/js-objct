5b8383c5025bd2c2620893bdd8211641619507b7
Philipp C. Adrian <philippcadrian@greenish.ch>
2013-05-28 23:23:56 +0200

    Benchmarking pcaClass.js ca. 10x faster than Class.js

--- Class.js
+++ Class.js
@@ -2,13 +2,14 @@
 	var Class = function(child, abstract){
 		if(!(this instanceof Class)) return new Class(child);
 		var extending = [child];
+		var building = false;
 		var fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
 		var Executable = function(){
 			// throw("Classes need to be initiated with the new operator");
 			var Class = Executable.build(function(){
 				// All construction is actually done in the construct method
 				//if( this._abstract ) throw("Abstract class may not be constructed");
-
+				if(building) return;
 				//console.log(this);
 				if(this.construct) 
 					return this.construct.apply(this, arguments);
@@ -17,8 +18,9 @@
 			Class.prototype.constructor = Class;
 
 			Class.prototype.instanceof = function(child){
+				if(Executable === child) return true;
 				for(var i=extending.length-1; i>=0; i--) {
-					if(extending[i] == child) return true;
+					if(extending[i] === child) return true;
 					if(extending[i].isInstance && extending[i].isInstance(child)) return true;
 				}
 				return false;
@@ -36,14 +38,19 @@
 			for(var i=extending.length-1; i>=0; i--) {
 				if(extending[i].build) Class=extending[i].build(Class);
 				else {
+					// building = true;
+					// Class = new Class();
+					// Class.prototype = {}
+					// building = false;
 					var properties = new extending[i]();
 					for (var name in properties) {
-						
-						// Check if we're overwriting an existing function
-						Class.prototype[name] = typeof properties[name] == "function" ?
-							(function(fn, _super,name) {
+						// Check if we have to wrap the function
+						var check=typeof properties[name] === "function" && (typeof extending[i].prototype[name] !== "function" || (typeof Class.prototype[name] === "function" && fnTest.test(properties[name])));
+			//			console.log(name, check, properties)
+						Class.prototype[name] = check ?
+							(function(fn, _super ,name) {
 								return function() {
-									if(!fnTest.test(fn) || !_super) _super = undefined;
+									//if(!fnTest.test(fn) || !_super) _super = undefined;
 									
 									var tmp = this._super;
 
--- pcaClass.js
+++ pcaClass.js
@@ -10,49 +10,51 @@
 	 */
 	var initializing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
 
-
 	// The base Class implementation (does nothing)
 	this.Class = function(){};
+
 	// Create a new Class that inherits from this class
-	var _extend = function(_super, child) {
-		var prototype= {}
+	Class._extend = function(prop) {
+		var _super = this.prototype;
 
 		// Instantiate a base class (but only create the instance,
 		// don't run the init constructor)
 		initializing = true;
-		_super = new _super();
-		child = new child();
+		prop = new prop();
+		var prototype = new this();
 		initializing = false;
 
 		// Copy the properties over onto the new prototype
-		for (var name in _super) {
-			console.log("super", name, typeof _super[name] == "function")
+		for (var name in prop) {
 			// Check if we're overwriting an existing function
-			prototype[name] = _super[name];
+			prototype[name] = typeof prop[name] == "function" && typeof _super[name] == "function" && fnTest.test(prop[name]) ?
+				(function(name, fn, _super){
+					return function() {
+						var tmp = this._super;
 
-			// typeof _super[name] == "function" && _super[name].toString()==wrap().toString()?
-			// 	_super[name].rebind(_super) :
-			// 	_super[name];
-		}
-		for (var name in child) {
-			console.log("child", name)
-			// Check if we're overwriting an existing function
-			prototype[name] = typeof child[name] == "function" ?
-				wrap(child, name, _super[name] && fnTest.test(child[name]) ? _super : undefined) :
-				child[name];
+						// Add a new ._super() method that is the same method
+						// but on the super-class
+						this._super = _super[name];
+
+						// The method only need to be bound temporarily, so we
+						// remove it when we're done executing
+						var ret = fn.apply(this, arguments);        
+						this._super = tmp;
+
+						return ret;
+					};
+				})(name, prop[name], _super) :
+				prop[name];
 		}
 
 		// The dummy class constructor
 		function Class() {
-			// All construction is actually done in the construct method
+			// All construction is actually done in the init method
 			if( initializing ) return
-		//	if( this._abstract ) throw("Abstract class may not be constructed");
-			else if( this.construct ) {
-				return this.construct.apply(this, arguments);
-			}
+			if( this._abstract ) throw("Abstract class may not be constructed"); //pca
+			else if( this.construct ) this.construct.apply(this, arguments);
 		}
 
-		console.log(prototype);
 		// Populate our constructed prototype object
 		Class.prototype = prototype;
 
@@ -60,58 +62,31 @@
 		Class.prototype.constructor = Class;
 
 		// And make this class extendable
+		Class._extend = arguments.callee;
 		Class.extend = extend.bind(Class);
 		Class.extend.abstract = abstract.bind(Class);
- 
-		// console.log("pcaClass.prototype",Class.prototype);
-		// console.log("pcaClass instance",new Class());
-		console.log("////////////",Class);
+		
+
 		return Class;
 	};
-	var wrap = function(child, name, _super){
-		var wrapper= function(closure) {
-			if(_super) {
-				var tmp = closure._super;
-
-				// Add a new ._super() method that is the same method
-				// but on the super-class
-				closure._super = _super[name];
-			}
-
-			// The method only need to be bound temporarily, so we
-			// remove it when we're done executing
-			var ret = child[name].apply(closure, arguments); 
-			if(_super) closure._super = tmp;
-			return ret;
-		}
-		var ret = function(){
-			return wrapper(this);
-		}
-		ret.rebind = function(closure) {
-			return function() {
-				debugger;
-				//rebound
-				return wrapper(closure);
-			}
-		}
-		return ret;
-	}
 
 	var extend = function(newClass){
 		this.prototype._abstract=false;
-		return _extend(this,newClass);
+		return this._extend(newClass);		
 	}
 	var abstract = function(newClass){
-		newClass.prototype._abstract= true;
-		return _extend(this,newClass);
+		this.prototype._abstract=true;
+		return this._extend(newClass);		
 	}
 	var pcaClass = function(newClass){
-		return _extend(Class, newClass);
+		return Class._extend(newClass);;
 	}
+
 	pcaClass.abstract = function(newClass) {
 		newClass.prototype._abstract= true;
 		return pcaClass(newClass);
 	}
+
 	pcaClass.extend = function(abstractClass, extendingClass) {
 		return typeof(abstractClass.extend) === "function" ?
 			abstractClass.extend(extendingClass):
