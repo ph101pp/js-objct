988a953777ae27afcdf0b3b9a8550e1e65f6e44a
Philipp C. Adrian <philippcadrian@greenish.ch>
2013-05-29 20:29:05 +0200

    Awesome improvements..

--- Class.js
+++ Class.js
@@ -1,47 +1,55 @@
 (function(undefined){
 	var building = false;
 	var fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
-	var Class = function(child, _abstract){
+	var Inheritance = function(child, _super, _abstract){
 		var extending = [];
-		var abstract = false;
-		var extend = function(child, _abstract) {
-			for(name in child) {
-				if(["_build", "extend"].indexOf(name) < 0) 
-					Executable[name]=child[name];
-				else if(''+child !== ''+Executable) 
-					throw("Method names '_build' and 'extend' are reserved on Class Objects (sorry)");
-			}
-			extending.push(child);
-			abstract = _abstract;
+		var abstract = _abstract || false;
+		var extend = function(child, _super) {
+			if(child._abstract) abstract = child._abstract();
+			if(_super && !_super._abstract() && abstract) throw("Only abstract classes can be extended by abstract classes.");
+
+			var classes = [_super, child];		
+			for(var i=0; i<classes.length; i++)
+				if(classes[i]) {
+					for(name in classes[i]) {
+						if(["_build", "extend", "_abstract"].indexOf(name) < 0) 
+							Executable[name]=classes[i][name];
+						else if(''+classes[i] !== ''+Executable) 
+							throw("Property names '_build', 'extend', '_instanceof' and '_abstract' are reserved on Class objects. (Sorry)");
+					}
+					extending.push(classes[i]);
+				}
 			return Executable;
 		}
-		var instanceOf = function(child){
-			if(Executable === child) return true;
-			for(var i=extending.length-1; i>=0; i--) {
-				if(extending[i] === child) return true;
-				if(extending[i].isInstance && extending[i].isInstance(child)) return true;
-			}
-			return false;
-		}
 		var Executable = function(){
-			if(!(this instanceof Executable)) throw("Classes need to be initiated with the new operator");
+			if(!(this instanceof Executable)) throw("Classes need to be initiated with the new operator.");
 
 			var Class = Executable._build(function Class(){
 				// All construction is actually done in the construct method
 				if(building) return;
-				if(abstract) throw("Abstract class may not be constructed");
+				if(abstract) throw("Abstract class may not be constructed.");
 				if(this.construct) return this.construct.apply(this, arguments);
 			});
 
 			// Add substitution for native instanceof operator
-			if(typeof Class.prototype.instanceof !== "function") Class.prototype.instanceof = instanceOf;
-			else Class.prototype._instanceof = instanceOf;
+			if(typeof Class.prototype.instanceof !== "function") Class.prototype.instanceof = Executable._instanceof;
+			else Class.prototype._instanceof = Executable._instanceof;
 			
 			// Initialize newly build Class with proper attributes
 			Array.prototype.unshift.call(arguments, null);
 			return new (Function.prototype.bind.apply(Class, arguments)); 	
 		}
+
 		Executable._build = function(Class){
+			// Define _instanceof function for every Executable that gets build.
+			Executable._instanceof = function(child){
+				if(Executable === child) return true;
+				for(var i=0; i<extending.length; i++) {
+					if(extending[i]._instanceof && extending[i]._instanceof(child)) return true;
+					else if(extending[i] === child) return true
+				}
+				return false;
+			};
 			for(var i=0; i<extending.length; i++) {
 				if(''+extending[i] === ''+Executable) Class=extending[i]._build(Class);
 				else {
@@ -56,8 +64,6 @@
 						Class.prototype[name] = check ?
 							(function(fn, _super) {
 								return function() {
-									//if(!fnTest.test(fn) || !_super) _super = undefined;
-									
 									var tmp = this._super;
 
 									// Add a new ._super() method that is the same method
@@ -73,25 +79,28 @@
 							})(properties[name], Class.prototype[name]) :
 							properties[name];
 					}
+					// Enforce the constructor to be what we expect
+					Class.prototype.constructor = Class;
 				}
 			}
-			// Enforce the constructor to be what we expect
-			Class.prototype.constructor = Class;
 			return Class;
 		}
+		Executable._abstract = function(){
+			return abstract
+		}
 		Executable.extend = function(child){
-			return extend(child, false);
+			return new Inheritance(child, Executable);
 		}		
 		Executable.extend.abstract = function(child){
-			return extend(child, true);
+			return new Inheritance(child, Executable, true);
 		}	
-		return extend(child, _abstract);
+		return extend(child, _super);
 	}
-	var Constructor = function(child){
-		return new Class(child, false);
+	var Class = function(child){
+		return new Inheritance(child);
 	}
-	Constructor.abstract = function(child){
-		return new Class(child, true);
+	Class.abstract = function(child){
+		return new Inheritance(child, undefined, true);
 	}
-	module.exports = Constructor;
+	module.exports = Class;
 })();
