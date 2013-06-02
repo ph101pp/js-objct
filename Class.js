40b9c57364056ed388c33230fba689a224956579
Philipp C. Adrian <philippcadrian@greenish.ch>
2013-06-02 17:51:38 +0200

    Some improvements to Class.js (Zwischenschritt)

--- Class.js
+++ Class.js
@@ -35,52 +35,57 @@
 		}
 		var Executable = function(){
 			if(!(this instanceof Executable)) throw("Classes need to be initiated with the new operator.");
+			if(abstract) throw("Abstract class may not be constructed.");
 
-			var Class = Executable._build(function Class(){
-				// All construction is actually done in the construct method
-				if(building) return;
-				if(abstract) throw("Abstract class may not be constructed.");
-				if(this.construct) return this.construct.apply(this, arguments);
-			});
-
-			// Add substitution for native instanceof operator
-			if(typeof Class.prototype.instanceof !== "function") Class.prototype.instanceof = Executable._instanceof;
-			else Class.prototype._instanceof = Executable._instanceof;
+			Array.prototype.unshift.call(arguments, null);
+			var Class = Executable._build(undefined, arguments);
 			
 			// Initialize newly build Class with proper attributes
-			Array.prototype.unshift.call(arguments, null);
-			return new (Function.prototype.bind.apply(Class, arguments)); 	
+			var instance = new (Function.prototype.bind.apply(Class, arguments)); 
+
+			// Add substitution for native instanceof operator
+			if(typeof instance.instanceof !== "function") instance.instanceof = Executable._instanceof;
+			else instance._instanceof = Executable._instanceof;
+
+			Array.prototype.shift.call(arguments);
+			if(instance.construct) var construct = instance.construct.apply(instance, arguments);
+			return ["object", "function"].indexOf(construct) < 0 ?
+				instance : construct;
 		}
-		Executable._build = function(Class){
+		Executable._build = function(Class, args){
 			// Define _instanceof function for every Executable that gets build.
 			Executable._instanceof = function(child){
 				if(Executable === child) return true;
 				for(var i=0; i<extending.length; i++) {
 					if(extending[i]._instanceof && extending[i]._instanceof(child)) return true;
 					else if(extending[i] === child) return true
-				}
+				} 
 				return false;
 			};
 			for(var i=0; i<extending.length; i++) {
-				if(typeof extending[i] === "function" && typeof extending[i]._build === "function") Class=extending[i]._build(Class);
+				if(typeof extending[i] === "function" && typeof extending[i]._build === "function") Class=extending[i]._build(Class,args);
 				else {
-					building = true;
-					Class.prototype = new Class();
-					building = false;
-
-					var properties = typeof extending[i] === "function" ?
-						new extending[i]():
+					if(typeof Class === "undefined") {
+						var Class= function(){};
+						Class.prototype = extending[i].prototype.__proto__;
+					}
+					var prototype = new Class;
+					
+					var instance = typeof extending[i] === "function" ?
+						new (Function.prototype.bind.apply(extending[i], args)):
 						extending[i];
 
-					for (var name in properties) {
+					//console.log(bla);
+					for (var name in instance) {
+					//	console.log(name);
 						// Check if we have to wrap the function either because it has a _super function or because it needs its own private closure.
-						var check=typeof properties[name] === "function" && ((extending[i].prototype && typeof extending[i].prototype[name] !== "function") || (typeof Class.prototype[name] === "function" && fnTest.test(properties[name])));
-						Class.prototype[name] = check ?
-							attachSuper(properties[name], Class.prototype[name]) :
-							properties[name];
+						prototype[name] = typeof instance[name] === "function" && instance[name] !== prototype[name] && typeof prototype[name] === "function" && fnTest.test(instance[name]) ?
+							attachSuper(instance[name], prototype[name]) :
+							instance[name];
 					}
 					// Enforce the constructor to be what we expect
-					Class.prototype.constructor = Class;
+					Class.prototype = prototype;
+					Class.prototype.constructor = extending[i];
 				}
 			}
 			return Class;
