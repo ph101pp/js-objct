0460a189395aa05e7b929ad36fd72ef457ad8f15
Philipp Adrian <philipp.adrian@dowjones.com>
2015-05-12 23:10:15 -0400

    tweaks

--- e/index.js
+++ e/index.js
@@ -1,6 +1,5 @@
 var objct = module.exports = require("../lib/objct.e");
-console.log(objct.e);
 
-// objct.e = new objct.extend(function(){return objct.e;}, {
+objct.e = new objct.extend(function(){return objct.e;}, {
 
-// });
+});
