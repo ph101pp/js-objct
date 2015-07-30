8bf0e3b27d3c8cb62ef5792a969840cae9afebc3
Philipp Adrian <philipp.adrian@dowjones.com>
2015-07-30 11:08:58 -0400

    tweak

--- e/index.js
+++ e/index.js
@@ -1,5 +1,5 @@
 var objct = module.exports = require("../objct");
 
-objct.e = new objct.extend(function(){return objct.e;}, {
+new objct.extend(function(){return objct.e;}, {
   deep : require("./deep")
 });
